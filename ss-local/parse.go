4823e1e963e81b29bf9489b8647e23b4225f120b
zsh <1026860069@qq.com>
2019-11-30 16:32:20 +0800

    edit

--- ss-local/parse.go
+++ ss-local/parse.go
@@ -4,9 +4,21 @@ import (
 	"bytes"
 	"encoding/base64"
 	"github.com/zshorz/shadowsockets/ss"
+	"log"
+	"net"
 	"strconv"
 	"strings"
 )
+
+type ServerCipher struct {
+	server string
+	cipher *ss.Cipher
+}
+
+var servers struct {
+	srvCipher []*ServerCipher
+	failCnt   []int
+}
 
 // 返回 host  其余自动设置
 func parseURI(u string, cfg *ss.Config) (string, error) {
@@ -55,4 +67,69 @@ func parseURI(u string, cfg *ss.Config) (string, error) {
 	}
 	cfg.ServerPort = p
 	return string(tailParts[0]), nil
+}
+
+func parseServerConfig(config *ss.Config) {
+	hasPort := func(s string) bool {
+		_, port, err := net.SplitHostPort(s)
+		if err != nil {
+			return false
+		}
+		return port != ""
+	}
+
+	if len(config.ServerPassword) == 0 {
+		// 只有一个加密表
+		cipher, err := ss.NewCipher(config.Method, config.Password)
+		if err != nil {
+			log.Fatal("Failed generating ciphers:", err)
+		}
+		srvPort := strconv.Itoa(config.ServerPort)
+		srvArr := config.GetServerArray()
+		n := len(srvArr)
+		servers.srvCipher = make([]*ServerCipher, n)
+		for i, s := range srvArr {
+			if hasPort(s) {
+				log.Println("ignore server_port option for server", s)
+				servers.srvCipher[i] = &ServerCipher{s, cipher}
+			} else {
+				servers.srvCipher[i] = &ServerCipher{net.JoinHostPort(s, srvPort), cipher}
+			}
+		}
+	} else {
+		// multiple servers
+		n := len(config.ServerPassword)
+		servers.srvCipher = make([]*ServerCipher, n)
+
+		cipherCache := make(map[string]*ss.Cipher)
+		i := 0
+		for _, serverInfo := range config.ServerPassword {
+			if len(serverInfo) < 2 || len(serverInfo) > 3 { // 只能是 2 或 3
+				log.Fatalf("server %v syntax error\n", serverInfo)
+			}
+			server := serverInfo[0]
+			passwd := serverInfo[1]
+			encmethod := ""
+			if len(serverInfo) == 3 {
+				encmethod = serverInfo[2]
+			}
+			cacheKey := encmethod + "|" + passwd
+			cipher, ok := cipherCache[cacheKey]
+			if !ok {
+				var err error
+				cipher, err = ss.NewCipher(encmethod, passwd)
+				if err != nil {
+					log.Fatal("Failed generating ciphers:", err)
+				}
+				cipherCache[cacheKey] = cipher
+			}
+			servers.srvCipher[i] = &ServerCipher{server, cipher}
+			i++
+		}
+	}
+	servers.failCnt = make([]int, len(servers.srvCipher))
+	for _, se := range servers.srvCipher {
+		log.Printf("available remote server", se.server)
+	}
+	return
 }
